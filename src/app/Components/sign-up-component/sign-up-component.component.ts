103e3f8906dd574ba13c6e065525324911cd2144
Donovan Bradley <dbjpk12@gmail.com>
2019-12-09 23:39:42 -0700

    I added a user.ts in the model folder and wrote some code for the signup form in the ts file. Currently in progress.

--- src/app/Components/sign-up-component/sign-up-component.component.ts
+++ src/app/Components/sign-up-component/sign-up-component.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../model/user';
+import { HttpClient } from '@angular/common/http';
+import { Input } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up-component',
@@ -9,14 +13,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignUpComponentComponent implements OnInit {
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  user: User;
+  @Input() signupPath: string;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  // User signup and storing the user in MongoDB
+  signup(user: User): Observable<User> {
+    return this.http.post<User>(this.signupPath, user);
+  }
+
+
 
   ngOnInit() {
+
+
     this.myForm = this.fb.group({
       email: '',
       username: '',
       password: ''
-    })
+    });
 
 
     this.myForm = this.fb.group({
@@ -33,10 +49,13 @@ export class SignUpComponentComponent implements OnInit {
         Validators.required,
         Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')
       ]]
-    })
-  } 
+    });
 
-  //Regex error handling
+    
+
+  }
+
+  // Regex error handling
   get email() {
     return this.myForm.get('email');
   }
@@ -49,8 +68,8 @@ export class SignUpComponentComponent implements OnInit {
     return this.myForm.get('username');
   }
 
-  //Test submit 
-  onSubmit(data:any):void {
+  // Test submit
+  onSubmit(data: any): void {
     console.log(data.value);
   }
 
