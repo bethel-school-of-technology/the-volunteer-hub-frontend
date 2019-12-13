a91f01fa477fa7d32e02b2efa9654a7a7e53df29
Elya <elyamurza@yahoo.com>
2019-12-13 13:54:08 -0800

    Organizations route, signup form
    Made it so that organizations are displayed when /organizations route is viewed,
    Sign up form restyling

--- src/app/Components/organizations/organizations.component.ts
+++ src/app/Components/organizations/organizations.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Organizations } from '../../model/organizations';
+
 
 @Component({
   selector: 'app-organizations',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organizations.component.css']
 })
 export class OrganizationsComponent implements OnInit {
+  organization: Organizations[];
+  _url: string = 'http://localhost:3001/getOrgs';
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   ngOnInit() {
+    this._http.get<Organizations[]>(this._url).subscribe(organization => {
+      this.organization = organization
+    });
   }
 
 }
---
+++ src/app/model/organizations.ts
@@ -0,0 +1,8 @@
+export class Organizations {
+    username: string;
+    name: string;
+    city: string;
+    state: string;
+    email: string;
+    phoneNumber: string;
+}
