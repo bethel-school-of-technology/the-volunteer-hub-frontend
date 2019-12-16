7d47bc9bbc08b873cdf2f49e6927e79d4f6538d4
hdreyer <hannahdreyer@gmail.com>
2019-12-16 17:01:44 -0600

    Update login user component code

--- src/app/Components/loginuser/loginuser.component.ts
+++ src/app/Components/loginuser/loginuser.component.ts
@@ -1,55 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
- 
-import { AuthenticationService } from '../../Services/authentication.service';
 
 @Component({
-  selector: 'app-loginuser',
-  templateUrl: './loginuser.component.html',
-  styleUrls: ['./loginuser.component.css']
 })
 export class LoginuserComponent implements OnInit {
   loginForm: FormGroup;
   loading = false;
   submitted = false;
   returnUrl: string;
-   
   constructor(
-  private formBuilder: FormBuilder,
-  private route: ActivatedRoute,
-  private router: Router,
-  private authenticationService : AuthenticationService
-  ) { }
-   
   ngOnInit() {
-  this.loginForm = this.formBuilder.group({
-  username: ['', Validators.required],
-  password: ['', Validators.required]
-  });
-   
   }
-   
   // for accessing to form fields
-  get fval() { return this.loginForm.controls; }
-   
-  onFormSubmit() {
-  this.submitted = true;
-  if (this.loginForm.invalid) {
-  return;
-  }
-   
-  this.loading = true;
-  this.authenticationService.login(this.fval.username.value, this.fval.password.value)
-  .subscribe(
-  data => {
-  this.router.navigate(['/']);
-  console.log("login successful");
-  },
-  error => {
-  alert ('Error');
-  this.loading = false;
-  });
   }
   }
-   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
-import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PostsComponent } from './Components/posts/posts.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 
 
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-
-import { LoginuserComponent } from './Components/loginuser/loginuser.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
 
 
 @NgModule({
@@ -24,8 +7,6 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
     AppComponent,
     PostsComponent,
     routingComponents,
-    LoginuserComponent,
-
     LoginuserComponent,
     NavbarComponent
   ],
@@ -43,6 +24,4 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
     MatChipsModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
 })
-export class AppModule { }
