56b06b1084366526fb6738542ac852a4e4daa5ab
hdreyer <hannahdreyer@gmail.com>
2019-12-16 17:03:23 -0600

    Update login user form code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,21 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PostsComponent } from "./Components/posts/posts.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
 
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
 
+import { LoginuserComponent } from "./Components/loginuser/loginuser.component";
+import { NavbarComponent } from "./Components/navbar/navbar.component";
 
 @NgModule({
   declarations: [
@@ -24,4 +39,6 @@
     MatChipsModule
   ],
   providers: [],
+  bootstrap: [AppComponent]
 })
+export class AppModule {}
