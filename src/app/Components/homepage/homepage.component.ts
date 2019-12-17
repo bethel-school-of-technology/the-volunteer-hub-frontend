423ce4bac4bc48161aa5697978953b7bbdf622c4
Elya <elyamurza@yahoo.com>
2019-12-16 20:01:35 -0800

    created search bar

--- src/app/Components/homepage/homepage.component.ts
+++ src/app/Components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  searchForm: FormGroup;
+  
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.searchForm = this.fb.group({
+      state: ['', Validators.required]
+    })
   }
 
+  //access form fields
+  get getFormValues() {
+    return this.searchForm.controls;
+  }
+
+  search(){
+    console.log(this.getFormValues.state.value);
+  }
 }
