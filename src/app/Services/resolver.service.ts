dedd735c240d7e4ee224886cdf53a78e5e4be847
Donovan Bradley <dbjpk12@gmail.com>
2019-12-27 22:47:27 -0700

    Attempting to make the create organiztion form work on the profile page of a signed in user.

--- src/app/Services/resolver.service.ts
+++ src/app/Services/resolver.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
+import { Organizations } from '../model/organizations';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -11,7 +12,12 @@ export class ResolverService implements Resolve<User> {
 
   constructor(private http: HttpClient) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):Observable<User> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User> {
     return this.http.get<User>('http://localhost:3001/users/Userprofile', { withCredentials: true });
   }
+
+  resolveOrg(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Organizations> {
+    return this.http.post<Organizations>('http://localhost:3001/users/createOrg', { withCredentials: true});
+  }
+
 }
