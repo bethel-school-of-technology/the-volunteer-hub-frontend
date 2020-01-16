17e1bd9872ebacde2abd463edb32ee7e54c14bd1
hdreyer <hannahdreyer@gmail.com>
2020-01-15 22:08:14 -0600

    Update admin

--- src/app/Components/admin/admin.component.ts
+++ src/app/Components/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Post } from "../../model/post";
 import { Organizations } from "../../model/organizations";
-import { User } from '../../model/user';
+import { User } from "../../model/user";
 
 @Component({
   selector: "app-admin",
@@ -10,10 +10,9 @@ import { User } from '../../model/user';
   styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
-
-  private orgsUrl = 'http://localhost:3001/getOrgs';
-  private usersUrl = 'http://localhost:3001/getUsers';
-  private deleteOrgUrl = 'http://localhost:3001/users/admin/deleteOrg';
+  private orgsUrl = "http://localhost:3001/getOrgs";
+  private usersUrl = "http://localhost:3001/getUsers";
+  private deleteOrgUrl = "http://localhost:3001/users/admin/deleteOrg";
   posts: Post[];
   organizations: Organizations[];
   org: Organizations;
@@ -25,25 +24,31 @@ export class AdminComponent implements OnInit {
     // this.http.get<Post[]>(this.dataPath).subscribe(posts => {
     //   this.posts = posts;
     // });
-    this.http.get<Organizations[]>(this.orgsUrl).subscribe(organizations => {
-      this.organizations = organizations;
-    });
     this.http.get<User[]>(this.usersUrl).subscribe(users => {
       this.users = users;
+    });
+    this.loadOrganizations();
+  }
+
+  loadOrganizations() {
+    this.http.get<Organizations[]>(this.orgsUrl).subscribe(organizations => {
+      this.organizations = organizations;
     });
   }
 
   deleteOganizationUrl(event: any) {
     console.log(event);
-    return this.http.delete<any>(`${this.deleteOrgUrl}/${event}`, { withCredentials: true });
+    return this.http.delete<any>(`${this.deleteOrgUrl}/${event}`, {
+      withCredentials: true
+    });
   }
 
   deleteOrganization(event: any) {
-    this.deleteOganizationUrl(event).subscribe(
-      deleted => {
-        alert('This organization has been deleted.' + '' + deleted.message);
-        // location.assign('http://localhost:4200/profile');
-      }
-    );
+    this.deleteOganizationUrl(event).subscribe(deleted => {
+      this.loadOrganizations();
+
+      alert("This organization has been deleted." + "" + deleted.message);
+      // location.assign('http://localhost:4200/profile');
+    });
   }
 }
