59f722c53a49ac1abc92719917017077513d2199
Elya <elyamurza@yahoo.com>
2020-01-21 16:23:59 -0800

    navbar responsive

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -15,4 +15,13 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.authenticationService.logout();
   }
+
+  myFunction() {
+    var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+  }
 }
